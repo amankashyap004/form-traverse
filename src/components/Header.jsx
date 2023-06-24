ac5601bb3e668b5b72728a5b9689e644420e261e
amankashyap004 <amankashyap004@gmail.com>
2023-06-24 20:17:18 +0530

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 const Header = () => {
-   return <div>Header</div>;
+   const navItems = ["Home", "About", "Contact"];
+
+   return (
+      <AppBar position="static">
+         <Toolbar className="bg-white">
+            <Typography variant="h6" className="text-black">
+               Form Traverse
+            </Typography>
+            <div className="flex ml-auto">
+               {navItems.map((item) => (
+                  <Typography
+                     key={item}
+                     variant="h6"
+                     className="text-black px-2 hover:underline cursor-pointer"
+                  >
+                     {item}
+                  </Typography>
+               ))}
+            </div>
+         </Toolbar>
+      </AppBar>
+   );
 };
 
 export default Header;
