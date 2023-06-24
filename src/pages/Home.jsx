5d99653b9bea0942d884fb644a09aa513ed52915
amankashyap004 <amankashyap004@gmail.com>
2023-06-24 20:38:19 +0530

    home page contain

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Home = () => {
-   return <div>Home</div>;
+   return (
+      <main className="flex justify-start items-center m-4">
+         <div className="w-1/2 p-4">
+            <p className="text-lg font-bold text-slate-700 ">
+               Unlock the door to a seamless online experience with Form Traverse, where form
+               filling becomes a journey of simplicity and efficiency.
+            </p>
+         </div>
+      </main>
+   );
 };
 
 export default Home;
