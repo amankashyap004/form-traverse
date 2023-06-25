388c5c47e7de927fbed5362b52f2f0064f0c7d93
amankashyap004 <amankashyap004@gmail.com>
2023-06-25 20:12:47 +0530

    create step progress

---
+++ src/components/StepProgress.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const StepProgress = () => {
+   const [activeStep, setActiveStep] = useState(1);
+
+   const handleClick = (step) => {
+      setActiveStep(step);
+   };
+
+   return (
+      <section className="flex justify-start items-start flex-col">
+         <div className="flex justify-center items-center mb-4">
+            <StepNumberIndicator step={1} activeStep={activeStep} onClick={handleClick} />
+            <StepNumberIndicator step={2} activeStep={activeStep} onClick={handleClick} />
+            <StepNumberIndicator step={3} activeStep={activeStep} onClick={handleClick} />
+            <StepNumberIndicator step={4} activeStep={activeStep} onClick={handleClick} />
+         </div>
+         <div className="mt-2 text-slate-500 text-sm">
+            <StepParagraph activeStep={activeStep} />
+         </div>
+      </section>
+   );
+};
+
+const StepNumberIndicator = ({ step, activeStep, onClick }) => {
+   const isActive = step === activeStep;
+
+   const handleClick = () => {
+      onClick(step);
+   };
+
+   return (
+      <div
+         className={`${
+            isActive ? "bg-yellow-400 text-black" : "text-slate-500 bg-slate-200"
+         } flex justify-center items-center text-sm mr-6 cursor-pointer w-8 h-8 rounded-full`}
+         onClick={handleClick}
+      >
+         {step}
+      </div>
+   );
+};
+
+const StepParagraph = ({ activeStep }) => {
+   const paragraphs = [
+      "Step 1 - Select Service Details",
+      "Step 2 - Add Contact Information",
+      "Step 3 - Add Service Address",
+      "Step 4 - Save Quote or Book Now",
+   ];
+
+   return <p>{paragraphs[activeStep - 1]}</p>;
+};
+
+export default StepProgress;
--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import CloseIcon from "@mui/icons-material/Close";
+import StepProgress from "../components/StepProgress";
 
 const FormPage = ({ isFormPage, setIsFormPage }) => {
    return (
       <>
          {isFormPage ? (
             <div className="bg-white shadow-md w-[92%] h-full absolute top-0 mx-8 ">
-               <div
+               <section
                   className="absolute right-2 top-2 px-4 py-2 cursor-pointer"
                   onClick={() => setIsFormPage(!isFormPage)}
                >
                   <CloseIcon style={{ fontSize: "30px" }} />
-               </div>
+               </section>
+               <section className="ml-12 mt-8 absolute">
+                  <StepProgress />
+               </section>
             </div>
          ) : (
             ""
