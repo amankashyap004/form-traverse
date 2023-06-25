5269431f096ff8ed4ba0d192410dae4781f6eed5
amankashyap004 <amankashyap004@gmail.com>
2023-06-25 18:52:02 +0530

    form page features

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
-const FormPage = () => {
-   return <div>FormPage</div>;
+const FormPage = ({ isFormPage, setIsFormPage }) => {
+   return (
+      <>
+         {isFormPage ? (
+            <div className="bg-white shadow-md w-[92%] h-full absolute top-0 mx-8 ">
+               <div
+                  className="absolute right-2 top-2 px-4 py-2 cursor-pointer"
+                  onClick={() => setIsFormPage(!isFormPage)}
+               >
+                  <CloseIcon style={{ fontSize: "30px" }} />
+               </div>
+            </div>
+         ) : (
+            ""
+         )}
+      </>
+   );
 };
 
 export default FormPage;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicBtn from "../components/BasicBtn";
+import FormPage from "./FormPage";
 
 const Home = () => {
+   const [isFormPage, setIsFormPage] = useState(false);
+
    return (
-      <main className="flex justify-between items-center m-4">
-         <section className="flex justify-center items-center flex-col p-4 w-1/2">
-            <div className="pb-4">
-               <p className="text-lg font-bold text-slate-700 ">
-                  Unlock the door to a seamless online experience with Form Traverse, where form
-                  filling becomes a journey of simplicity and efficiency.
+      <main className="m-4">
+         <section className="flex justify-between items-center">
+            <section className="flex justify-center items-center flex-col p-4 w-1/2">
+               <div className="pb-4">
+                  <p className="text-lg font-bold text-slate-700 ">
+                     Unlock the door to a seamless online experience with Form Traverse, where form
+                     filling becomes a journey of simplicity and efficiency.
+                  </p>
+               </div>
+               <div className="mt-8" onClick={() => setIsFormPage(!isFormPage)}>
+                  <BasicBtn contained="BOOKING CLEANING" />
+               </div>
+            </section>
+            <div className="p-4 w-1/2">
+               <p className="text-base font-semibold text-slate-500 text-justify">
+                  Form Traverse is a modern and intuitive multi-step form system I have built using
+                  React. With Form Traverse, I aim to enhance the user experience of form filling by
+                  breaking down lengthy or complex forms into manageable sections. By dividing the
+                  form into multiple pages, users can comfortably complete one page at a time and
+                  seamlessly navigate to the next. My solution not only simplifies the form-filling
+                  process but also provides a clear and structured user interface, ensuring that
+                  users can easily track their progress. With Form Traverse, I strive to make form
+                  submission a breeze, offering an efficient and user-friendly experience for all.
                </p>
             </div>
-            <div className="mt-8">
-               <BasicBtn contained="BOOKING CLEANING" />
-            </div>
          </section>
-         <div className="p-4 w-1/2">
-            <p className="text-base font-semibold text-slate-500 text-justify">
-               Form Traverse is a modern and intuitive multi-step form system I have built using
-               React. With Form Traverse, I aim to enhance the user experience of form filling by
-               breaking down lengthy or complex forms into manageable sections. By dividing the form
-               into multiple pages, users can comfortably complete one page at a time and seamlessly
-               navigate to the next. My solution not only simplifies the form-filling process but
-               also provides a clear and structured user interface, ensuring that users can easily
-               track their progress. With Form Traverse, I strive to make form submission a breeze,
-               offering an efficient and user-friendly experience for all.
-            </p>
-         </div>
+         {isFormPage ? <FormPage isFormPage={isFormPage} setIsFormPage={setIsFormPage} /> : ""}
       </main>
    );
 };
