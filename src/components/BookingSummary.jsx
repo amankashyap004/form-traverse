2721cc681223dfa6818f753eb92ae16166a75cc0
amankashyap004 <amankashyap004@gmail.com>
2023-06-29 19:55:58 +0530

    some modification

--- src/components/BookingSummary.jsx
+++ src/components/BookingSummary.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import { useSelector } from "react-redux";
 import SelectedMultiOptions from "./steps/SelectedMultiOptions";
 import { getSelectedOptionLabel } from "../store/selectors/selectors";
+import HouseOutlinedIcon from "@mui/icons-material/HouseOutlined";
 
 const BookingSummary = () => {
    const selectedOptionLabel = useSelector(getSelectedOptionLabel);
    return (
-      <section>
-         <h1 className="font-extrabold text-3xl border-b border-slate-400 pb-4 w-full">
-            Booking summary
-         </h1>
-         <div className="py-4">Booking summary contain</div>
-         <div>
-            <p>Selected Option: {selectedOptionLabel}</p>
+      <section className="w-[90%]">
+         <h1 className="font-extrabold text-3xl border-b border-slate-400 pb-4">Booking summary</h1>
+         <div className="flex justify-start items-center py-4">
+            <HouseOutlinedIcon sx={{ fontSize: 40 }} />
+            <p className="ml-2 text-base font-semibold">{selectedOptionLabel}</p>
          </div>
          <div className="pb-4 w-full">
             <SelectedMultiOptions />
--- src/components/steps/SelectedMultiOptions.jsx
+++ src/components/steps/SelectedMultiOptions.jsx
@@ -7,10 +7,10 @@ const SelectedMultiOptions = () => {
    return (
       <div>
          {selectedMultiOptions.length > 0 && (
-            <div className="flex justify-start items-center flex-col max-h-48 overflow-y-auto my-2 p-2 border-y border-slate-400">
+            <div className="flex justify-start items-center flex-col max-h-48 overflow-y-auto my-2 px-2 py-4 border-y border-slate-400">
                {selectedMultiOptions.map((option) => (
                   <div key={option.value} className="flex justify-between items-center w-full my-1">
-                     <span>{option.label}</span>
+                     <span className="text-base font-semibold">{option.label}</span>
                      <span className="font-black">{option.price}</span>
                   </div>
                ))}
