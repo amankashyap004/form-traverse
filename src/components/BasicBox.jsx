e4621e1e988c09a9372c2050ed32dd63113ccfff
amankashyap004 <amankashyap004@gmail.com>
2023-06-29 21:40:11 +0530

    add some features in house details

--- src/components/BasicBox.jsx
+++ src/components/BasicBox.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import HomeWorkOutlinedIcon from "@mui/icons-material/HomeWorkOutlined";
 
-const BasicBox = ({ icon, title, paragraph, isSelected }) => {
+const BasicBox = ({ data, isSelected, onClick }) => {
+   const handleClick = () => {
+      onClick(data);
+   };
+
    return (
       <div
          className={`flex justify-center items-center flex-col w-28 py-2 mr-2 rounded-lg border border-slate-400 cursor-pointer ${
             isSelected ? "bg-slate-300" : ""
          }`}
-         onClick={() => isSelected}
+         onClick={handleClick}
       >
-         <div>{icon ? icon : <HomeWorkOutlinedIcon sx={{ fontSize: 40 }} />}</div>
-         <span className="font-bold text-base">{title}</span>
-         <span className="text-xs font-semibold text-slate-500">{paragraph}</span>
+         <div>{data.icon ? data.icon : <HomeWorkOutlinedIcon sx={{ fontSize: 40 }} />}</div>
+         <span className="font-bold text-base">{data.title}</span>
+         <span className="text-xs font-semibold text-slate-500">{data.paragraph}</span>
       </div>
    );
 };
--- src/components/steps/HouseDetails.jsx
+++ src/components/steps/HouseDetails.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicBox from "../BasicBox";
 
+const dataOfHouseDetails = [
+   {
+      key: "1",
+      icon: "",
+      title: "1be / 1ba",
+      paragraph: "< 1000 sq. ft.",
+      label: "La Petite - 1 to 2 Bedroom Condo (under 1000 sq. ft.)",
+   },
+   {
+      key: "2",
+      icon: "",
+      title: "2be / 2ba",
+      paragraph: "> 1000 sq. ft.",
+      label: "La Petite II - 2 Bedroom 2 Bath Home/Condo (1001-1500 sq. ft.)",
+   },
+   {
+      key: "3",
+      icon: "",
+      title: "3be / 2ba",
+      paragraph: "< 1800 sq. ft.",
+      label: "Le Milieu - 3 Bedroom 2 Bath Home (under 1800 sq. ft.)",
+   },
+];
+
 const HouseDetails = () => {
+   const [selectedBox, setSelectedBox] = useState(null);
+
+   const handleBoxClick = (data) => {
+      setSelectedBox(data);
+   };
+
    return (
       <div className="flex justify-start items-center">
-         <BasicBox title="1bhk/2bhk" paragraph="<1000 sq. ft." onClick={""} />
+         {dataOfHouseDetails.map((data) => (
+            <BasicBox
+               key={data.key}
+               data={data}
+               isSelected={selectedBox === data}
+               onClick={handleBoxClick}
+            />
+         ))}
       </div>
    );
 };
