01033f574b4d03c54813dac8d67a8939cf8c67e3
amankashyap004 <amankashyap004@gmail.com>
2023-06-30 19:23:04 +0530

    create service frequency feature in extra service details

--- src/components/steps/ExtraServiceDetails.jsx
+++ src/components/steps/ExtraServiceDetails.jsx
@@ -4,25 +4,49 @@ import CustomDropdown from "../CustomDropdown";
 import { setSelectedTimingData } from "../../store/actions/actions";
 import data from "../../data.json";
 
+const serviceFrequencyData = [
+   {
+      value: "option1",
+      label: "One Time Cleaning",
+   },
+   {
+      value: "option2",
+      label: "Weekly Cleaning (20%)",
+   },
+];
+
 const ExtraServiceDetails = () => {
    const dispatch = useDispatch();
    const timingData = data.timingData;
 
-   const handleDropdownChange = (option) => {
+   const handleDropdownChangeTiming = (option) => {
       dispatch(setSelectedTimingData(option.label));
+   };
+
+   const handleDropdownServiceFrequency = (option) => {
+      console.log(option.label);
    };
 
    return (
       <section className="flex justify-center items-center w-full">
+         {/* Date */}
          <div className="mb-4 w-1/3 mr-3"></div>
+         {/* Timing */}
          <div className="mb-4 w-1/3 mr-3">
             <CustomDropdown
                options={timingData}
-               onChange={handleDropdownChange}
+               onChange={handleDropdownChangeTiming}
                defaultValue="Select time"
             />
          </div>
-         <div className="mb-4 w-1/3"></div>
+         {/* Service Frequency */}
+         <div className="mb-4 w-1/3">
+            <CustomDropdown
+               options={serviceFrequencyData}
+               onChange={handleDropdownServiceFrequency}
+               defaultValue="Service Frequency"
+            />
+         </div>
       </section>
    );
 };
