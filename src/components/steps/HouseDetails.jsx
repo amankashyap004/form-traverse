6b370318163d4cee9067c357978af57309469f8e
amankashyap004 <amankashyap004@gmail.com>
2023-06-30 14:00:39 +0530

    modification house details data to dada.json

--- src/components/steps/HouseDetails.jsx
+++ src/components/steps/HouseDetails.jsx
@@ -3,32 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import BasicBox from "../BasicBox";
 import { setSelectedBoxData } from "../../store/actions/actions";
 import { getSelectedBoxData } from "../../store/selectors/selectors";
-
-const dataOfHouseDetails = [
-   {
-      key: "1",
-      icon: "",
-      title: "1be / 1ba",
-      paragraph: "< 1000 sq. ft.",
-      label: "La Petite - 1 to 2 Bedroom Condo (under 1000 sq. ft.)",
-   },
-   {
-      key: "2",
-      icon: "",
-      title: "2be / 2ba",
-      paragraph: "> 1000 sq. ft.",
-      label: "La Petite II - 2 Bedroom 2 Bath Home/Condo (1001-1500 sq. ft.)",
-   },
-   {
-      key: "3",
-      icon: "",
-      title: "3be / 2ba",
-      paragraph: "< 1800 sq. ft.",
-      label: "Le Milieu - 3 Bedroom 2 Bath Home (under 1800 sq. ft.)",
-   },
-];
+import data from "../../data.json";
 
 const HouseDetails = () => {
+   const dataOfHouseDetails = data.dataOfHouseDetails;
    const dispatch = useDispatch();
    const [selectedBox, setSelectedBox] = useState(dataOfHouseDetails[0]);
 
