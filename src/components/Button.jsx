5b03ac4da1c44b7ab00ce58c90bbd18471837a64
amankashyap004 <amankashyap004@gmail.com>
2023-07-01 14:01:13 +0530

    some more css in button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,8 @@ const Button = ({ contained, disabled, onClick }) => {
    return (
       <button
          className={`px-20 py-3 font-semibold text-base rounded-full bg-blue-950 text-white uppercase cursor-pointer ${
-            disabled ? "opacity-50 text-slate-500" : ""
-         }`}
+            disabled ? "opacity-30 text-slate-500" : ""
+         }  ${contained === "Back" ? "bg-yellow-400 text-slate-900" : ""} `}
          disabled={disabled}
          onClick={onClick}
       >
