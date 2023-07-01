202d70e3c43863eb165e9391822da31bef1c16a1
amankashyap004 <amankashyap004@gmail.com>
2023-07-01 13:50:34 +0530

    some more feature in step progress

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Button = ({ contained, disabled }) => {
+const Button = ({ contained, disabled, onClick }) => {
    return (
       <button
          className={`px-20 py-3 font-semibold text-base rounded-full bg-blue-950 text-white uppercase cursor-pointer ${
             disabled ? "opacity-50 text-slate-500" : ""
          }`}
          disabled={disabled}
+         onClick={onClick}
       >
          {contained}
       </button>
--- src/components/steps/StepProgress.jsx
+++ src/components/steps/StepProgress.jsx
@@ -2,53 +2,56 @@ import React, { useState } from "react";
 import ServiceDetails from "./ServiceDetails";
 import ContactInformation from "./contactInformation/ContactInformation";
 import ServiceAddress from "./serviceAddress/ServiceAddress";
-
 import Button from "../Button";
 
 const StepProgress = () => {
    const [activeStep, setActiveStep] = useState(1);
 
-   const handleClick = (step) => {
-      setActiveStep(step);
+   const handleNextStep = () => {
+      if (activeStep < 4) {
+         setActiveStep(activeStep + 1);
+      }
+   };
+
+   const handlePreviousStep = () => {
+      if (activeStep > 1) {
+         setActiveStep(activeStep - 1);
+      }
    };
 
    return (
       <section className="flex justify-start items-start flex-col relative w-full">
          <div className="h-0.5 bg-slate-200 w-[10rem] top-4 left-2 absolute"></div>
          <div className="flex justify-center items-center absolute mb-4">
-            <StepNumberIndicator step={1} activeStep={activeStep} onClick={handleClick} />
-            <StepNumberIndicator step={2} activeStep={activeStep} onClick={handleClick} />
-            <StepNumberIndicator step={3} activeStep={activeStep} onClick={handleClick} />
-            <StepNumberIndicator step={4} activeStep={activeStep} onClick={handleClick} />
+            <StepNumberIndicator step={1} activeStep={activeStep} />
+            <StepNumberIndicator step={2} activeStep={activeStep} />
+            <StepNumberIndicator step={3} activeStep={activeStep} />
+            <StepNumberIndicator step={4} activeStep={activeStep} />
          </div>
          <div className="mt-14 text-slate-500 text-sm">
             <StepParagraph activeStep={activeStep} />
          </div>
          <div className="mt-4 w-full">
-            <ServiceDetails />
-            <ContactInformation />
-            <ServiceAddress />
+            {activeStep === 1 && <ServiceDetails />}
+            {activeStep === 2 && <ContactInformation />}
+            {activeStep === 3 && <ServiceAddress />}
          </div>
-         <div className="mt-4 w-full">
-            <Button contained="Next Step" disabled={false} />
+         <div className="flex justify-between items-center mt-4 w-full">
+            <Button contained="Back" disabled={activeStep === 1} onClick={handlePreviousStep} />
+            <Button contained="Next Step" disabled={activeStep === 4} onClick={handleNextStep} />
          </div>
       </section>
    );
 };
 
-const StepNumberIndicator = ({ step, activeStep, onClick }) => {
+const StepNumberIndicator = ({ step, activeStep }) => {
    const isActive = step === activeStep;
-
-   const handleClick = () => {
-      onClick(step);
-   };
 
    return (
       <div
          className={`${
             isActive ? "bg-yellow-400 text-black" : "text-slate-500 bg-slate-200"
          } flex justify-center items-center text-sm mr-6 cursor-pointer w-8 h-8 rounded-full`}
-         onClick={handleClick}
       >
          {step}
       </div>
