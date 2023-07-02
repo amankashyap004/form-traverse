d193ff648da8408844e53245f25112f2f4aa3cb3
amankashyap004 <amankashyap004@gmail.com>
2023-07-02 16:29:47 +0530

    add basic info in contact information

--- src/components/steps/contactInformation/ContactInformation.jsx
+++ src/components/steps/contactInformation/ContactInformation.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const ContactInformation = () => {
-   return <div>ContactInformation</div>;
+   return (
+      <section>
+         <div className=" flex justify-start items-start flex-col">
+            <span className="text-4xl font-extrabold my-4">Your Contact Details</span>
+            <span className="text-lg font-bold my-2">
+               Please provide your name, phone number and email
+            </span>
+         </div>
+      </section>
+   );
 };
 
 export default ContactInformation;
