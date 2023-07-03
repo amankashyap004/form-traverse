e95845d10724c04a2467f9e75447f5d2cee4aea0
amankashyap004 <amankashyap004@gmail.com>
2023-07-03 18:47:52 +0530

    add basic feature in service address

--- src/components/steps/serviceAddress/ServiceAddress.jsx
+++ src/components/steps/serviceAddress/ServiceAddress.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ServiceAddress = () => {
-   return <div>ServiceAddress</div>;
+   return (
+      <section>
+         <div className=" flex justify-start items-start flex-col">
+            <span className="text-4xl font-extrabold my-4">Where Do You Need Service?</span>
+            <span className="text-lg font-bold my-2">Service Address</span>
+         </div>
+      </section>
+   );
 };
 
 export default ServiceAddress;
