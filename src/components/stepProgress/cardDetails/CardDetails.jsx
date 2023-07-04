714cde62be925d61e12c8b1e8c79280aecbce9ac
amankashyap004 <amankashyap004@gmail.com>
2023-07-04 12:44:26 +0530

    basic info in card details

--- src/components/stepProgress/cardDetails/CardDetails.jsx
+++ src/components/stepProgress/cardDetails/CardDetails.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const CardDetails = () => {
-   return <div>CardDetails</div>;
+   return (
+      <section>
+         <div className=" flex justify-start items-start flex-col">
+            <span className="text-4xl font-extrabold my-4">Book Now or Save Quote</span>
+            <span className="text-base font-semibold my-2">
+               You may book now by entering your card details or leave blank and save quote for
+               later.
+            </span>
+         </div>
+      </section>
+   );
 };
 
 export default CardDetails;
