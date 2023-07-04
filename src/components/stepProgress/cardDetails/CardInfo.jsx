750f874c6a78e9b4bd79d546150a8ce4210726d6
amankashyap004 <amankashyap004@gmail.com>
2023-07-04 17:46:50 +0530

    add some more feature in card info

--- src/components/stepProgress/cardDetails/CardInfo.jsx
+++ src/components/stepProgress/cardDetails/CardInfo.jsx
@@ -13,11 +13,27 @@ const CardInfo = () => {
 
       const numericValue = value.replace(/\D/g, "");
 
-      let updatedValue = numericValue;
-      if (name === "cardNumber" && numericValue.length > 16) {
-         updatedValue = numericValue.slice(0, 16);
-      } else if (name === "cardDate" && numericValue.length > 4) {
-         updatedValue = numericValue.slice(0, 4);
+      let formattedValue = "";
+      if (name === "cardDate") {
+         if (numericValue.length > 2) {
+            formattedValue = numericValue.substr(0, 2) + "/" + numericValue.substr(2, 2);
+         } else {
+            formattedValue = numericValue;
+         }
+      } else {
+         for (let i = 0; i < numericValue.length; i += 4) {
+            if (i !== 0) {
+               formattedValue += " ";
+            }
+            formattedValue += numericValue.substr(i, 4);
+         }
+      }
+
+      let updatedValue = formattedValue;
+      if (name === "cardNumber" && formattedValue.length > 19) {
+         updatedValue = formattedValue.slice(0, 19);
+      } else if (name === "cardDate" && formattedValue.length > 5) {
+         updatedValue = formattedValue.slice(0, 5);
       } else if (name === "cardCvv" && numericValue.length > 3) {
          updatedValue = numericValue.slice(0, 3);
       }
@@ -41,7 +57,7 @@ const CardInfo = () => {
             />
             <CustomizeInput
                type="text"
-               placeholder="00/00"
+               placeholder="MM/YY"
                name="cardDate"
                value={formData.cardDate}
                onChange={handleInputChange}
