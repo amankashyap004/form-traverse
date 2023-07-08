2e63d3edcd7eada82fe2aecf0f149df7641d84c6
amankashyap004 <amankashyap004@gmail.com>
2023-07-08 18:06:15 +0530

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 
 function App() {
    return (
-      <BrowserRouter>
+      <BrowserRouter basename="/form-traverse">
          <Header />
          <Routes>
             <Route path="/" element={<Home />} />
