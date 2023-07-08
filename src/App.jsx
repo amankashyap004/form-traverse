c77142ebc7df9a911f67e7cad70cbe1d5aba2a89
amankashyap004 <amankashyap004@gmail.com>
2023-07-08 17:50:50 +0530

    setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
 function App() {
    return (
-      <>
+      <BrowserRouter>
          <Header />
-         <Home />
-      </>
+         <Routes>
+            <Route path="/" element={<Home />} />
+         </Routes>
+      </BrowserRouter>
    );
 }
 
