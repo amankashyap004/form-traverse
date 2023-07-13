3df6a93a6678235661524a99988df47e75f5a6bd
amankashyap004 <amankashyap004@gmail.com>
2023-07-13 17:38:28 +0530

    add more feature in save quote

--- src/components/stepProgress/cardDetails/SaveQuote.jsx
+++ src/components/stepProgress/cardDetails/SaveQuote.jsx
@@ -5,18 +5,27 @@ import {
    getSelectedOptionData,
    getSelectedBoxData,
    getSelectedMultiOptions,
+   getSelectedDateData,
+   getSelectedTimingData,
+   getServiceFrequencyData,
 } from "../../../store/selectors/selectors";
 
 const SaveQuote = () => {
    const selectedOptionData = useSelector(getSelectedOptionData);
    const selectedBoxData = useSelector(getSelectedBoxData);
    const selectedMultiOptions = useSelector(getSelectedMultiOptions);
+   const selectedDateData = useSelector(getSelectedDateData);
+   const selectedTimeData = useSelector(getSelectedTimingData);
+   const serviceFrequencyData = useSelector(getServiceFrequencyData);
 
    const handleSaveQuote = () => {
       console.log("Save Quote");
       console.log(selectedOptionData.label + selectedOptionData.price);
       console.log(selectedBoxData.label + selectedBoxData.price);
       console.log(selectedMultiOptions);
+      console.log(selectedDateData);
+      console.log(selectedTimeData);
+      console.log(serviceFrequencyData.label + serviceFrequencyData.offer);
    };
 
    return (
