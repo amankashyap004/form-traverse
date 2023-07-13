ce1c5d1cbe54bf1de49e93c0e59745c8a82b5823
amankashyap004 <amankashyap004@gmail.com>
2023-07-13 17:22:13 +0530

    get selected option data in save quote

--- src/components/stepProgress/cardDetails/SaveQuote.jsx
+++ src/components/stepProgress/cardDetails/SaveQuote.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Button from "../../Button";
+import { getSelectedOptionData } from "../../../store/selectors/selectors";
 
 const SaveQuote = () => {
+   const selectedOptionData = useSelector(getSelectedOptionData);
+
    const handleSaveQuote = () => {
       console.log("Save Quote");
+      console.log(selectedOptionData.label + selectedOptionData.price);
    };
+
    return (
       <Button
          extraStyle="bg-blue-950 ml-4 text-white sm:ml-2"
