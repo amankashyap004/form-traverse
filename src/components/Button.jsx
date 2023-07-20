4ce9d9d0aa2c60f7a102dd649d611151dd79569b
amankashyap004 <amankashyap004@gmail.com>
2023-07-20 17:59:46 +0530

    some modification

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ contained, disabled, onClick, extraStyle }) => {
    return (
       <button
-         className={`px-16 py-3 font-semibold text-base rounded-full uppercase cursor-pointer md:px-12 sm:px-6 sm:text-sm ${
+         className={`px-16 py-3 font-semibold text-base rounded-full uppercase cursor-pointer md:px-12 sm:px-4 sm:text-xs ${
             disabled ? "opacity-30 text-slate-500" : ""
          } ${extraStyle ? extraStyle : ""}`}
          disabled={disabled}
